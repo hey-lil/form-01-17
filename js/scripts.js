034ae3a81a9dca4e3cefb544f36683382b5cebfc
hey-lil <lillian.r.taft@gmail.com>
2020-01-17 11:04:04 -0800

    connecting css, js, and html for initial background color branching

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  
+  $(".astroBranch.form-control#aries").click(function() {
+    $("body").removeClass();
+    $("body").addClass("#aries");
+    $("body").show(".quiz")
+  });
